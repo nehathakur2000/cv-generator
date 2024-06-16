8a6917ce3360b275bc88d8deadf9529c08ab3858
nehathakur2000 <148423767+nehathakur2000@users.noreply.github.com>
2024-06-16 18:43:28 +0530

    Add files via upload

---
+++ cvgenerator.py
@@ -0,0 +1,13 @@
+# from docx import Document
+
+# doc = Document()
+# name = input("Enter your name:")
+
+# doc.add_paragraph(f"Name{name}")
+# age= input("Enter your age:")
+# doc.add_paragraph(f"Age{age}")
+# file_name = input("enter the file name :")
+# doc.save(file_name)
+# print("done")
+
+
